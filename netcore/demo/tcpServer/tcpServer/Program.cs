8a6b294ad20597829610845c0cf836ef44eef6fd
levon.chen <levon.chen@foxmail.com>
2018-05-28 12:14:24 +0800

    add tcp server files

---
+++ netcore/demo/tcpServer/tcpServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tcpUtil.Util myUtil = new tcpUtil.Util();
+            myUtil.X = 60;
+            Console.WriteLine("Hello World! My Util is: " + myUtil.X );
+        }
+    }
+}
---
+++ netcore/demo/tcpServer/tcpUtil/Util.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace tcpUtil
+{
+    public class Util
+    {
+        public int X { get; set; }
+    }
+}
