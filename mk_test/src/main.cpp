6098ca8f900a311bafed48c1e363d2fbae7286fb
levon.chen <levon.chen@foxmail.com>
2018-09-11 15:18:25 +0800

    add test files

---
+++ mk_test/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#include "functions.h"
+
+int main()
+{
+    print_hello();
+    count << endl;
+    count << "The factorial of 5 is " << factorial(5) <<endl;
+
+    return 0
+}
